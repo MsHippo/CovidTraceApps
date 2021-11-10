6e949b126aab3c4799bff225b4b634fa24c31a46
MsHippo <syewhei@gmail.com>
2021-11-10 21:27:58 +0800

    the update side navigation

---
+++ CovidTrace/app/src/main/java/com/coffee/covidtrace/Ui/VaccinationActivity.java
@@ -0,0 +1,16 @@
+package com.coffee.covidtrace.Ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.coffee.covidtrace.R;
+
+public class VaccinationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vaccination);
+    }
+}
