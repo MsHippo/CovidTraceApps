da7fad220d890b3e4b8104f7e162d28880899dfc
MsHippo <syewhei@gmail.com>
2021-11-22 00:20:13 +0800

    modify the side navigation

---
+++ CovidTrace/app/src/main/java/com/coffee/covidtrace/Ui/ReportCaseActivity.java
@@ -0,0 +1,16 @@
+package com.coffee.covidtrace.Ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.coffee.covidtrace.R;
+
+public class ReportCaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_case);
+    }
+}
