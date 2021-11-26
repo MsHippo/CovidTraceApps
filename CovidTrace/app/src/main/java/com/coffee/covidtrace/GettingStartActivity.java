0fc51f3189df62ee0846e70cb024deb1561df8a5
MsHippo <syewhei@gmail.com>
2021-11-26 16:02:13 +0800

    tidy up the code

--- CovidTrace/app/src/main/java/com/coffee/covidtrace/GettingStartActivity.java
+++ CovidTrace/app/src/main/java/com/coffee/covidtrace/GettingStartActivity.java
@@ -1,6 +1,8 @@
 package com.coffee.covidtrace;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +12,17 @@ public class GettingStartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_getting_start);
+
+        // toolbar is defined in the layout file
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Getting Started");
+        setSupportActionBar(toolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        assert ab != null;
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
