f3b72557e1f148c4fb22393833f95f8299c4c21f
MsHippo <syewhei@gmail.com>
2021-12-01 23:38:03 +0800

    searching function and marker function done

--- CovidTrace/app/src/main/java/com/coffee/covidtrace/Ui/hotspot/HotspotActivity.java
+++ CovidTrace/app/src/main/java/com/coffee/covidtrace/Ui/hotspot/HotspotActivity.java
@@ -5,28 +5,39 @@ import static com.coffee.covidtrace.Constants.MAPVIEW_BUNDLE_KEY;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
 
 
 import com.coffee.covidtrace.R;
 import com.coffee.covidtrace.Ui.record.SuccessCheckInActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 
 public class HotspotActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     //widget
     private MapView mMapView;
-
+    private SearchView searchView;
+    GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +47,7 @@ public class HotspotActivity extends AppCompatActivity implements OnMapReadyCall
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         mMapView = findViewById(R.id.map);
+        searchView = findViewById(R.id.search_location);
 
 
         toolbar.setTitle("COVID-19 Hotspot Tracker");
@@ -49,6 +61,36 @@ public class HotspotActivity extends AppCompatActivity implements OnMapReadyCall
         ab.setDisplayHomeAsUpEnabled(true);
 
         initGoogleMap(savedInstanceState);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location  = searchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (location!=null || !location.equals("")){
+                    Geocoder geocoder = new Geocoder(HotspotActivity.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(location,1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    assert addressList != null;
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    googleMap.addMarker(new MarkerOptions()
+                            .position(latLng).title(location)
+                            .draggable(false).visible(true));
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
     }
 
@@ -73,7 +115,7 @@ public class HotspotActivity extends AppCompatActivity implements OnMapReadyCall
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
+    public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
 
         Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
@@ -105,6 +147,9 @@ public class HotspotActivity extends AppCompatActivity implements OnMapReadyCall
 
     @Override
     public void onMapReady(@NonNull GoogleMap map) {
+
+        googleMap = map;
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -118,6 +163,7 @@ public class HotspotActivity extends AppCompatActivity implements OnMapReadyCall
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+
         map.setMyLocationEnabled(true);
     }
 
