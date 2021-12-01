6513559ce3f20d56d20d7638f735f110dc5274e9
MsHippo <syewhei@gmail.com>
2021-12-01 21:15:49 +0800

    insert the map into the cardview using mapview

--- CovidTrace/app/src/main/java/com/coffee/covidtrace/Ui/hotspot/HotspotActivity.java
+++ CovidTrace/app/src/main/java/com/coffee/covidtrace/Ui/hotspot/HotspotActivity.java
@@ -1,27 +1,43 @@
 package com.coffee.covidtrace.Ui.hotspot;
 
+import static com.coffee.covidtrace.Constants.MAPVIEW_BUNDLE_KEY;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
 
 import com.coffee.covidtrace.R;
 import com.coffee.covidtrace.Ui.record.SuccessCheckInActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 
-public class HotspotActivity extends AppCompatActivity {
+public class HotspotActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    //widget
+    private MapView mMapView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotspot);
 
-        // my_child_toolbar is defined in the layout file
+
         Toolbar toolbar = findViewById(R.id.toolbar);
+        mMapView = findViewById(R.id.map);
+
+
         toolbar.setTitle("COVID-19 Hotspot Tracker");
         setSupportActionBar(toolbar);
 
@@ -32,11 +48,94 @@ public class HotspotActivity extends AppCompatActivity {
         assert ab != null;
         ab.setDisplayHomeAsUpEnabled(true);
 
+        initGoogleMap(savedInstanceState);
+
+    }
+
+    private void initGoogleMap(Bundle savedInstanceState){
+        // *** IMPORTANT ***
+        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+        // objects or sub-Bundles.
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+
+        mMapView.onCreate(mapViewBundle);
+
+        mMapView.getMapAsync(this);
     }
 
     public void searchHotspotArea(View view) {
         Intent intent = new Intent(this, HotSpotResultActivity.class);
 //        intent.putExtra("LOCATION_RESULTS", String.valueOf(location));
         startActivity(intent);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mMapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap map) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        map.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
