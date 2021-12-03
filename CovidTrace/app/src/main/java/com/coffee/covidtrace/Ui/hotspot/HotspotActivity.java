235e6b82b0aec6fd3458af71c1480efb05cf05de
MsHippo <syewhei@gmail.com>
2021-12-03 20:44:27 +0800

    add to show the current location name on the screen and read the user's latitude and longitude which can be used to later db

--- CovidTrace/app/src/main/java/com/coffee/covidtrace/Ui/hotspot/HotspotActivity.java
+++ CovidTrace/app/src/main/java/com/coffee/covidtrace/Ui/hotspot/HotspotActivity.java
@@ -16,16 +16,23 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationRequest;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.ResultReceiver;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import com.coffee.covidtrace.R;
 import com.coffee.covidtrace.Ui.record.SuccessCheckInActivity;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -50,20 +57,30 @@ public class HotspotActivity extends AppCompatActivity implements OnMapReadyCall
     private GoogleMap googleMap;
     private Button btn_auto_focus;
     private FusedLocationProviderClient mlocationClient;
+    private TextView txtUserCurrentLocation;
+    private TextView txtTitleLocation;
+    private ProgressBar progressBar;
     Location currentLocation;
     private static final int REQUEST_CODE = 101;
+    private ResultReceiver resultReceiver;
+
+    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
+    private long FASTEST_INTERVAL = 2000; /* 2 sec */
 
     @SuppressLint("VisibleForTests")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotspot);
-
+        resultReceiver = new AddressResultReceiver(new Handler());
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         mMapView = findViewById(R.id.map);
         searchView = findViewById(R.id.search_location);
         btn_auto_focus = findViewById(R.id.btn_auto_focus);
+        txtUserCurrentLocation = findViewById(R.id.txt_user_current_location);
+        txtTitleLocation = findViewById(R.id.txt_title_location);
+        progressBar = findViewById(R.id.progress_bar);
 
 
         toolbar.setTitle("COVID-19 Hotspot Tracker");
@@ -83,9 +100,23 @@ public class HotspotActivity extends AppCompatActivity implements OnMapReadyCall
         btn_auto_focus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getCurrentLoc();
+
+                if (ActivityCompat.checkSelfPermission(HotspotActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    getCurrentLoc();
+                }
+                else
+                {
+                    //if permission denied
+                    //Request Permission
+                    ActivityCompat.requestPermissions(HotspotActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+
+                }
+
             }
         });
+
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -122,22 +153,61 @@ public class HotspotActivity extends AppCompatActivity implements OnMapReadyCall
 
     private void getCurrentLoc() {
 
-
+        progressBar.setVisibility(View.VISIBLE);
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]
                     { Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
             return;
         }
+
+        LocationServices.getFusedLocationProviderClient(HotspotActivity.this)
+                .requestLocationUpdates(locationRequest, new LocationCallback(){
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        LocationServices.getFusedLocationProviderClient(HotspotActivity.this)
+                                .removeLocationUpdates(this);
+                        if (locationResult!=null && locationResult.getLocations().size()>0){
+                            int latestLocationIndex = locationResult.getLocations().size()-1;
+
+                            //get current latitude and longitude of the user
+                            double latitude = locationResult.getLocations().get(latestLocationIndex).getLatitude();
+                            double longitude = locationResult.getLocations().get(latestLocationIndex).getLongitude();
+
+                            Location location = new Location("providerNA");
+                            location.setLatitude(latitude);
+                            location.setLongitude(longitude);
+                            fetchAddressFromLatLng(location);
+                        }
+                        else{
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                }, Looper.getMainLooper());
+
         Task<Location> task = mlocationClient.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
+
+                //when success
                 if (location!=null){
                     currentLocation = location;
                     Toast.makeText(getApplicationContext(), currentLocation.getLatitude()
                             + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
 
-                    goToLocation(location.getLatitude(), location.getLongitude(),location.toString());
+                    mMapView.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(@NonNull GoogleMap googleMap) {
+
+                            //initialize lan lng
+                            goToLocation(location.getLatitude(), location.getLongitude(), String.valueOf(location.getAccuracy()));
+                        }
+                    });
                 }
             }
         });
@@ -151,13 +221,44 @@ public class HotspotActivity extends AppCompatActivity implements OnMapReadyCall
 //        });
     }
 
+    private void fetchAddressFromLatLng(Location location){
+        Intent intent = new Intent(this, FetchAddressIntentService.class);
+        intent.putExtra(Constants.RECEIVER, resultReceiver);
+        intent.putExtra(Constants.LOCATION_DATA_EXTRA, location);
+        startService(intent);
+    }
+
+    private class AddressResultReceiver extends ResultReceiver{
+        AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            super.onReceiveResult(resultCode, resultData);
+            if (resultCode == Constants.SUCCESS_RESULT){
+                txtTitleLocation.setVisibility(View.VISIBLE);
+                txtUserCurrentLocation.setVisibility(View.VISIBLE);
+                txtUserCurrentLocation.setText(resultData.getString(Constants.RESULT_DATA_KEY));
+            }else{
+                Toast.makeText(
+                        HotspotActivity.this,
+                        resultData.getString(Constants.RESULT_DATA_KEY),
+                        Toast.LENGTH_SHORT).show();
+            }
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
     private void goToLocation(double latitude, double longitude, String location) {
         LatLng latLng = new LatLng(latitude, longitude);
-        googleMap.addMarker(new MarkerOptions()
+        MarkerOptions markerOptions = new MarkerOptions()
                 .position(latLng).title(location)
-                .draggable(false).visible(true));
+                .draggable(false).visible(true);
+
 //        googleMap.addMarker(new MarkerOptions());
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+        googleMap.addMarker(markerOptions);
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 
@@ -217,22 +318,16 @@ public class HotspotActivity extends AppCompatActivity implements OnMapReadyCall
 
         googleMap = map;
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED
+//                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+////            ActivityCompat.requestPermissions(this, new String[]
+////                    { Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+//            return;
+//        }
 
-        map.setMyLocationEnabled(true);
-
+//        map.setMyLocationEnabled(true);
 
 
     }
@@ -253,5 +348,17 @@ public class HotspotActivity extends AppCompatActivity implements OnMapReadyCall
     public void onLowMemory() {
         super.onLowMemory();
         mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLoc();
+            }else {
+                Toast.makeText(this, "Permission denied.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
